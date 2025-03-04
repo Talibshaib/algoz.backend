f650ea34de1f6a59e6976dc419eca5aac2c2b0bd
talib <mdtalib23038@gmail.com>
2025-03-04 22:37:19 +0530

    chore: backend connection fixed

--- src/app.js
+++ src/app.js
@@ -4,41 +4,35 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://algoz-frontend.vercel.app",
+    "https://algoz-frontend-git-main-talibs-projects.vercel.app",
+    "https://algoz-frontend-talibs-projects.vercel.app",
+    "https://algoz-frontend.vercel.app"
+]
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || ["http://localhost:3000", "https://algoz-frontend.vercel.app"],
-    credentials: true
+    origin: function(origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
 })) 
-
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-
 //routes import
 import userRouter from './routes/user.routes.js'
-// import healthcheckRouter from "./routes/healthcheck.routes.js"
-// import tweetRouter from "./routes/tweet.routes.js"
-// import subscriptionRouter from "./routes/subscription.routes.js"
-// import videoRouter from "./routes/video.routes.js"
-// import commentRouter from "./routes/comment.routes.js"
-// import likeRouter from "./routes/like.routes.js"
-// import playlistRouter from "./routes/playlist.routes.js"
-// import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes declaration
-// app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter)
-// app.use("/api/v1/tweets", tweetRouter)
-// app.use("/api/v1/subscriptions", subscriptionRouter)
-// app.use("/api/v1/videos", videoRouter)
-// app.use("/api/v1/comments", commentRouter)
-// app.use("/api/v1/likes", likeRouter)
-// app.use("/api/v1/playlist", playlistRouter)
-// app.use("/api/v1/dashboard", dashboardRouter)
-
-// http://localhost:8000/api/v1/users/register
 
 export { app }
